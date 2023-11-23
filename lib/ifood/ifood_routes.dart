d3f228855e0a4c2281e9c9055fec1d0118438865
pedrovs2003 <118378102+pedrovs2003@users.noreply.github.com>
2023-11-23 19:43:55 -0300

    Tela inicial nova

--- lib/ifood/ifood_routes.dart
+++ lib/ifood/ifood_routes.dart
@@ -10,7 +10,7 @@ import 'package:flutter_application_1/ifood/ifood_perfil_drawer.dart';
 
 Map<String, WidgetBuilder> routes() {
   return {
-    '/': (context) => const IfoodEntrada(),
+    '/Login': (context) => const IfoodEntrada(),
     '/Entrada': (context) => const IfoodEntrada(),
     '/HomePage': (context) => const IfoodHomePage(),
     '/Perfil': (context) => const IfoodDrawer(),
